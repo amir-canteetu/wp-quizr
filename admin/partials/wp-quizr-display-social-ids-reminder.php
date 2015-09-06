ba044e4a3de45ca5a01885d666f4dcbdbab53e44
Amir Canteetu <amircanteetu@gmail.com>
2015-09-06 13:02:35 +0200

    Updated reminder text

--- admin/partials/wp-quizr-display-social-ids-reminder.php
+++ admin/partials/wp-quizr-display-social-ids-reminder.php
@@ -1,7 +1,7 @@
 
 <div class="inside">
     
-    <p class="metabox-p"><?php esc_html_e('Please provide at least one social network to which you would like users to share their quiz results. Go to Settings->WP Quizr Settings to do this. You will then be able to specify your quiz information.', 'wp-quizr'); ?></p>
+    <p class="metabox-p"><?php esc_html_e('Don\'t forget to provide at least one social network to which you would like users to share their quiz results. Go to Settings->WP Quizr Settings to do this.', 'wp-quizr'); ?></p>
 
 </div>
 
