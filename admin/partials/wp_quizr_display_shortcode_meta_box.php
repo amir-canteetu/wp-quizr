09bb756b925ee54b3e029db46bd8235429854c34
amir canteetu <amircanteetu@gmail.com>
2015-10-20 23:00:51 +0200

    Added shortcode reminder about column numbers

--- admin/partials/wp_quizr_display_shortcode_meta_box.php
+++ admin/partials/wp_quizr_display_shortcode_meta_box.php
@@ -1,7 +1,8 @@
 
       
-<p class="shortcode_notice"><?php esc_html_e('When you have finished creating your quiz, copy and paste the following code into the post or page in which you would like it to appear:', 'wp-quizr'); ?></p>
+<p class="shortcode_notice"><?php esc_html_e('When you have finished creating your quiz, copy and paste the following code into the post or page in which you would like it to appear.', 'wp-quizr'); ?></p>
+<p class="shortcode_notice"><?php esc_html_e('DON\'T FORGET TO REPLACE "X" WITH THE NUMBER OF COLUMNS YOU WOULD LIKE TO HAVE', 'wp-quizr'); ?></p>
+    
+    <?php
 
-<?php
-
-echo '<h2>[wp_quizr id = "' . $box['args']['id'] . '"]</h2>';
+echo '<h2>[wp_quizr id = "' . $box['args']['id'] . '"  columns = "X"]</h2>';
