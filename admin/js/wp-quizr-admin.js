113496050bbd28fbbe164f973e893aa154766aec
amir canteetu <amircanteetu@gmail.com>
2015-10-20 23:09:41 +0200

    Minified Js

--- admin/js/wp-quizr-admin.js
+++ admin/js/wp-quizr-admin.js
@@ -24,16 +24,10 @@
 
                 }
 
-                var newRow = jQuery('<p><label>Image For No Outcome</label><br><br><img style="max-width: 100%; height: auto;" src=""><input type="hidden" name="wp_quizr_question_' + wp_quizr_question_number + '_no_outcome_image_' + counter + '_url" id="wp_quizr_question_' + wp_quizr_question_number + '_no_outcome_image_' + counter + '_url" value=""><br><br><input type="button" class="wp_quizr_question_outcome_image_button" id="wp_quizr_question_' + wp_quizr_question_number + '_no_outcome_image_' + counter + '_button" value="Choose or Upload an Image"><br><br><input type="button" class="wp_quizr_question_outcome_remove_img_button" value="Remove Image"></p>');
+                var newRow = jQuery('<p><label>Image For No Outcome</label><br><br><img style="max-width: 100%; height: auto;" src=""><input type="hidden" name="wp_quizr_question_' + wp_quizr_question_number + '_no_outcome_image_' + counter + '_url" id="wp_quizr_question_' + wp_quizr_question_number + '_no_outcome_image_' + counter + '_url" value=""><br><br><input type="button" class="wp_quizr_question_outcome_image_button" id="wp_quizr_question_' + wp_quizr_question_number + '_no_outcome_image_' + counter + '_button" value="Choose or Upload an Image"><br><br><input type="button" class="wp_quizr_question_outcome_remove_img_button" value="Remove Image"><br><br><input class="widefat" type="text" name="wp_quizr_question_' + wp_quizr_question_number + '_no_outcome_' + counter + '_image_caption" id="wp_quizr_question_' + wp_quizr_question_number + '_no_outcome_' + counter + '_image_caption" placeholder="Image Caption" value="" size="30"></p>');
 
                 newRow.insertBefore(jQuery(this).parent().parent());
 
-                //remove handler if the number of no-outcome images is the same as the total number of outcomes; to prevent excess no-outcome images
-                if(counter == wp_quizr_number_of_outcomes) {
-
-                   jQuery(this).unbind("click"); 
-
-                }
             });        
         
         
